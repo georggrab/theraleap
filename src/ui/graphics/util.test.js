4ad10638ae15a71e8ff7a7cd84af5e9af6aceed3
talkdirty <talkdirty@users.noreply.github.com>
2018-03-25 12:50:17 +0200

    Remove auxiliary files

--- src/ui/graphics/util.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ava_1 = require("ava");
-ava_1.default('foo', function (t) {
-    t.pass();
-});
-//# sourceMappingURL=util.test.js.map
