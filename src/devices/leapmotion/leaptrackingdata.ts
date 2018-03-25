ab199c1592a63e44ca619b656f4b10122168c1af
talkdirty <talkdirty@users.noreply.github.com>
2018-03-25 12:48:33 +0200

    Add Types for Leap specific device tracking data

---
+++ src/devices/leapmotion/leaptrackingdata.ts
@@ -0,0 +1,65 @@
+export interface LeapDeviceFrame {
+  currentFrameRate: number;
+  devices: any[];
+  gestures: any[];
+  hands: LeapHand[];
+  id: number;
+  interactionBox: LeapInteractionBox;
+  pointables: LeapPointable[];
+  r: number[][];
+  s: number;
+  t: number[];
+  timestamp: number;
+}
+
+export interface LeapPointable {
+  bases: number[][][];
+  btipPosition: number[];
+  carpPosition: number[];
+  dipPosition: number[];
+  direction: number[];
+  extended: boolean;
+  handId: number;
+  id: number;
+  length: number;
+  mcpPosition: number[];
+  pipPosition: number[];
+  stabilizedTipPosition: number[];
+  timeVisible: number;
+  tipPosition: number[];
+  tipVelocity: number[];
+  tool: boolean;
+  touchDistance: number;
+  touchZone: string;
+  type: number;
+  width: number;
+}
+
+export interface LeapInteractionBox {
+  center: number[];
+  size: number[];
+}
+
+export interface LeapHand {
+  armBasis: number[][];
+  armWidth: number;
+  confidence: number;
+  direction: number[];
+  elbow: number[];
+  grabStrength: number;
+  id: number;
+  palmNormal: number[];
+  palmPosition: number[];
+  palmVelocity: number[];
+  palmWidth: number;
+  pinchStrength: number;
+  r: number[][];
+  s: number;
+  sphereCenter: number[];
+  sphereRadius: number;
+  stabilizedPalmPosition: number[];
+  t: number[];
+  timeVisible: number;
+  type: string;
+  wrist: number[];
+}
