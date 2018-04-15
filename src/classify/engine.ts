2e91fe7edaf4375eabcf66e62ef1f48b8973e9b5
talkdirty <talkdirty@users.noreply.github.com>
2018-04-15 10:19:00 +0200

    Add Threaded Worker Engine

---
+++ src/classify/engine.ts
@@ -0,0 +1,31 @@
+import { Observable, Subscription } from "@reactivex/rxjs";
+
+import { Classifier, ClassificationData } from "./classifier";
+import { GenericHandTrackingData } from "@/devices/generic";
+
+export abstract class ClassificationEngine {
+  public abstract handleResult(c: ClassificationData): void;
+
+  private classifiers: Classifier[] = [];
+  private classifierSubscriptions: Subscription[] = [];
+
+  public addClassifier(c: Classifier) {
+    this.classifiers.push(c);
+  }
+
+  public process(source: Observable<GenericHandTrackingData>) {
+    this.classifiers.forEach(classifier => {
+      const subscription = classifier
+        .classify(source)
+        .subscribe(this.handleResult.bind(this));
+      this.classifierSubscriptions.push(subscription);
+    });
+  }
+
+  public terminate() {
+    this.classifierSubscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
+    this.classifierSubscriptions = [];
+  }
+}
